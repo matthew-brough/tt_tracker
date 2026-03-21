63cf8e2952d8b760c03c6cde2acfe412dc478315
artemOP <26147889+artemOP@users.noreply.github.com.>
2026-03-21 09:56:44 +0000

    shrink chunk_time_interval from the standard

--- db/migrations/005_compression.sql
+++ db/migrations/005_compression.sql
@@ -1,5 +1,6 @@
 -- +goose NO TRANSACTION
 -- +goose Up
+
 ALTER TABLE player_positions SET (
     timescaledb.compress,
     timescaledb.compress_segmentby = 'server',
@@ -7,3 +8,4 @@ ALTER TABLE player_positions SET (
 );
 
 SELECT add_compression_policy('player_positions', INTERVAL '1 hour');
+SELECT set_chunk_time_interval('player_positions', INTERVAL '6 hours');
